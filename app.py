e8a64b677c6379e13fb13190b166219178f32ace
bennalex2014 <alexander.j.bennett3@gmail.com>
2024-11-11 21:31:14 -0500

    basic imports/setup + stubs for database
    recreate bool determines creation of database vs connection
    - default is set to true
    - once run for the first time add a gitignore for the sqllite3 file and set recreate to false

--- app.py
+++ app.py
@@ -1,3 +1,44 @@
-# TODO below
-    # database create and delete with starter data -> upon first start up -> set bool to create again
-    # stub different functions
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# true -> recreates db
+# false -> connects to existing db
+recreate = True
+
+# Determine the absolute path of our database file
+scriptdir = os.path.abspath(os.path.dirname(__file__))
+dbpath = os.path.join(scriptdir, 'freeGamesList.sqlite3')
+
+# Configure the Flask App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbpath}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Getting the database object handle from the app
+db = SQLAlchemy(app)
+
+# TODO define Model for Users
+class Users(db.Model):
+    __tablename__ = 'Users'
+    
+
+# TODO define Model for Games
+class Game(db.Model):
+    __tablename__ = 'Games'
+    
+
+# TODO define Model for Forums
+class Forum(db.Model):
+    __tablename__ = 'Forums'
+    
+
+if recreate:
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        
+        # TODO fill with API data
+
+
+# TODO stub different functions
